f38a098c39c1ac409c8a329d8361bf0c27590707
serjfri <malquebien@gmail.com>
2025-05-27 14:21:22 +0200

    Fix: Configurado basename en BrowserRouter para GitHub Pages.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/tarot-intuitive-guide"> {/* <--- ¡AQUÍ ESTÁ EL CAMBIO! */}
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
